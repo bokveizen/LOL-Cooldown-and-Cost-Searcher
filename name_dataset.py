79624f6b9ee7f50051803d3b370d45fa090569db
BoqVezen <boqvezen97@kaist.ac.kr>
2020-04-10 11:23:36 +0900

    old file

--- name_dataset.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-
-res = defaultdict(list)
-with open('namesets.txt', encoding='utf8') as f:
-    for line in f.readlines():
-        info = line[:-1].split(' ')
-        for i in range(1, len(info)):
-            res[info[0]].append(info[i])
